bc43c342e2eb3383f0e07fbea3be67a628924525
henriSedjame <sedhjo@gmail.com>
2018-10-27 01:06:39 +0200

    fin du mapping model -> paypal

--- src/main/java/fr/projects/online_checkout/core/model/Adresse.java
+++ src/main/java/fr/projects/online_checkout/core/model/Adresse.java
@@ -1,8 +1,12 @@
 package fr.projects.online_checkout.core.model;
 
-import lombok.Data;
+import lombok.*;
 
-@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
 public class Adresse {
   private String numero;
   private String rue;
